8ef835d7cd795b2645f196b35bac4f403bb796ef
Nils Müller <nils@techtales.io>
2022-02-04 14:36:47 +0100

    Fix: typo #66

--- infra/terraform/authentik/prometheus_auth_proxy.tf
+++ infra/terraform/authentik/prometheus_auth_proxy.tf
@@ -1,7 +1,7 @@
 // configure auth proxy
 resource "authentik_provider_proxy" "prometheus" {
   name               = "prometheus"
-  internal_host      = "http:// prometheus-prometheus.observability.svc.cluster.local:9090"
+  internal_host      = "http://prometheus-prometheus.observability.svc.cluster.local:9090"
   external_host      = "https://prometheus.${var.cloudflare_domain}"
   authorization_flow = data.authentik_flow.default_provider_authorization_explicit_consent.id
 }
