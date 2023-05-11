e2e22554b386f22f3542f38eb225e69488f04030
Nils Müller <tyriis@users.noreply.github.com>
2023-05-11 23:56:38 +0200

    ci(gh): implement minecraft bedrock updater #29

---
+++ .github/scripts/minecraftVersionCheck.mjs
@@ -0,0 +1,17 @@
+#!/usr/bin/env zx
+$.verbose = false
+
+const latestVersion = argv["version"]
+const helmRelease = argv["helm-release"]
+
+const yq = await which("yq")
+const yamlPath = ".spec.values.minecraftServer.version"
+
+// get the current bedrock version from yaml
+const result = await $`${yq} ${yamlPath} ${helmRelease}`
+const currentVersion = result.stdout.trim().replace(new RegExp('"', "g"), "")
+
+if (latestVersion !== currentVersion) {
+  await $`sed -i "s%${currentVersion}%${latestVersion}%g" ${helmRelease}`
+  echo(latestVersion)
+}
