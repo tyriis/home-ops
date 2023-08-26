07375bd5f768df146e5fa5b1e3f2fc2a4cec7c5b
Jazzlyn <9011011+jazzlyn@users.noreply.github.com>
2023-08-26 17:06:47 +0200

    feat(nix): add shell.nix

---
+++ shell.nix
@@ -0,0 +1,21 @@
+let
+  pkgs = import <nixpkgs> {};
+in
+  pkgs.mkShell {
+    packages = [
+      pkgs.age
+      pkgs.direnv
+      pkgs.go-task
+      pkgs.k9s
+      pkgs.kubectl
+      pkgs.kubernetes-helm
+      pkgs.pre-commit
+      pkgs.sops
+      pkgs.talosctl
+      pkgs.terraform
+      pkgs.terraform-docs
+      pkgs.tig
+      pkgs.tflint
+      pkgs.tfsec
+    ];
+  }
