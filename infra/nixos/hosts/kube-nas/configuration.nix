ecae696146e458e7bec5bcee399e72377ee69650
Nils Müller <tyriis@users.noreply.github.com>
2024-08-03 23:49:31 +0200

    fix(nixos): kube-nas k3s config prevent deployment of local-storage-provisioner

--- infra/nixos/hosts/kube-nas/configuration.nix
+++ infra/nixos/hosts/kube-nas/configuration.nix
@@ -13,6 +13,7 @@
       ./nfs.nix
       ../../users.nix
       ../../modules/neovim.nix
+      # ../../modules/k3s.nix
     ];
 
   # enable nix-flakes
@@ -169,18 +170,38 @@
   # Enable zsh.
   programs.zsh.enable = true;
 
+  # # Enable k3s
+  # services.k3s.enable = true;
+  # services.k3s.role = "server";
+  # services.k3s.extraFlags = toString [
+  #   "--flannel-backend=none" # https://docs.k3s.io/installation/network-options
+  #   "--disable=coredns" # https://docs.k3s.io/networking#coredns
+  #   "--disable=traefik" # https://docs.k3s.io/networking#traefik-ingress-controller
+  #   "--disable-network-policy" # https://docs.k3s.io/networking#network-policy-controller
+  #   "--disable=servicelb" # https://docs.k3s.io/networking#disabling-servicelb
+  #   "--disable=metrics-server" # https://docs.k3s.io/installation/packaged-components?_highlight=metric#using-the---disable-flag
+  #   "--disable local-storage"
+  # ];
+  # # service.k3s.extraManifests = {
+  # #   "local-storage-skip" = builtins.toFile "local-storage.yaml.skip" "";
+  # # };
+
   # Enable k3s
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    "--flannel-backend=none" # https://docs.k3s.io/installation/network-options
-    "--disable=coredns" # https://docs.k3s.io/networking#coredns
-    "--disable=traefik" # https://docs.k3s.io/networking#traefik-ingress-controller
-    "--disable-network-policy" # https://docs.k3s.io/networking#network-policy-controller
-    "--disable=servicelb" # https://docs.k3s.io/networking#disabling-servicelb
-    "--disable=metrics-server" # https://docs.k3s.io/installation/packaged-components?_highlight=metric#using-the---disable-flag
-    "--disable=local-storage"
-  ];
+  services.k3s = {
+    enable = true;
+    role = "server";
+    extraFlags = toString [
+      "--flannel-backend=none" # https://docs.k3s.io/installation/network-options
+      "--disable=coredns" # https://docs.k3s.io/networking#coredns
+      "--disable=traefik" # https://docs.k3s.io/networking#traefik-ingress-controller
+      "--disable-network-policy" # https://docs.k3s.io/networking#network-policy-controller
+      "--disable=servicelb" # https://docs.k3s.io/networking#disabling-servicelb
+      "--disable=metrics-server" # https://docs.k3s.io/installation/packaged-components?_highlight=metric#using-the---disable-flag
+      "--disable local-storage"
+    ];
+  };
+  environment.etc."rancher/k3s/server/manifests/local-storage.yaml.skip".text = "";
+
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
