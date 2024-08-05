b4ac80b5285162df9b4a4b16354f001c1bd2cd2a
Nils Müller <tyriis@users.noreply.github.com>
2024-08-06 00:02:00 +0200

    fix(nixos): adjust folder creation and typo

--- infra/nixos/hosts/kube-nas/configuration.nix
+++ infra/nixos/hosts/kube-nas/configuration.nix
@@ -28,7 +28,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   # btrfs
-  boot.supportedFilesystems = [ "btrfs", "xfs" ];
+  boot.supportedFilesystems = [ "btrfs" "xfs" ];
 
   nixpkgs.config.allowUnfree = true;
 
@@ -243,6 +243,7 @@
   # create folder
   # https://discourse.nixos.org/t/adding-folders-and-scripts/5114/4
   systemd.tmpfiles.rules = [
-    "d /mount/volume1/minio 0755 568 568" # 568 is the minio user
+    "d /mnt/volume1/data 0777 root root"
+    "d /mnt/volume1/minio 0755 568 568" # 568 is the minio user
   ];
 }
