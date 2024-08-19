bfd20c24b22f29f4ed914a36caab3b9d75b5bc9c
Nils Müller <tyriis@users.noreply.github.com>
2024-08-20 00:01:24 +0200

    feat(dev-tools): restore nix-shell for jazzlyn

---
+++ shell.nix
@@ -0,0 +1,26 @@
+{ pkgs ? import <nixpkgs> {}, lib ? pkgs.lib }:
+pkgs.mkShell {
+  packages = [
+    pkgs.age
+    pkgs.direnv
+    pkgs.fluxcd
+    pkgs.go-task
+    pkgs.k9s
+    pkgs.kubectl
+    pkgs.kubernetes-helm
+    pkgs.kustomize
+    pkgs.pre-commit
+    pkgs.gitleaks
+    pkgs.ydiff
+    pkgs.sops
+    pkgs.talosctl
+    pkgs.terraform
+    pkgs.terraform-docs
+    pkgs.tig
+    pkgs.tflint
+    pkgs.tfsec
+    pkgs.vault
+    pkgs.trivy
+    # (builtins.getFlake "github:budimanjojo/talhelper").packages.x86_64-linux.default
+  ];
+}
