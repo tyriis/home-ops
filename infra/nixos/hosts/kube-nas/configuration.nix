d53931c24354e6e02dda191fd9fea2c2d21256f3
Nils Müller <tyriis@users.noreply.github.com>
2024-10-27 19:22:55 +0100

    fix(nixos): adjust kube-nas config
    - You can't use ssh-agent and GnuPG agent with SSH support enabled at the same time!

--- infra/nixos/hosts/kube-nas/configuration.nix
+++ infra/nixos/hosts/kube-nas/configuration.nix
@@ -85,10 +85,10 @@
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
