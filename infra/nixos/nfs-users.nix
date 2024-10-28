b39b4ea101363290fdecf3311824469817273cd0
Nils Müller <tyriis@users.noreply.github.com>
2024-10-28 23:13:41 +0100

    fix(nixos): make nfs users normal users

--- infra/nixos/nfs-users.nix
+++ infra/nixos/nfs-users.nix
@@ -3,19 +3,19 @@
 {
   users.users.alex = {
     uid = 1002;
-    isSystemUser = true;
+    isNormalUser = true;
     createHome = false;
   };
 
   users.users.dominik = {
     uid = 1003;
-    isSystemUser = true;
+    isNormalUser = true;
     createHome = false;
   };
 
   users.users.kube = {
     uid = 1004;
-    isSystemUser = true;
+    isNormalUser = true;
     createHome = false;
   };
 }
