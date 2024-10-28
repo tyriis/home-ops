fe244c9fa5a180103830dc6b285b1a6e97f496fe
Nils Müller <tyriis@users.noreply.github.com>
2024-10-28 23:12:41 +0100

    fix(nixos): make nfs-users system users:

--- infra/nixos/nfs-users.nix
+++ infra/nixos/nfs-users.nix
@@ -3,19 +3,19 @@
 {
   users.users.alex = {
     uid = 1002;
-    isNormalUser = false;
+    isSystemUser = true;
     createHome = false;
   };
 
   users.users.dominik = {
     uid = 1003;
-    isNormalUser = false;
+    isSystemUser = true;
     createHome = false;
   };
 
   users.users.kube = {
     uid = 1004;
-    isNormalUser = false;
+    isSystemUser = true;
     createHome = false;
   };
 }
