7f850c87368e796d938010b61a285d9ab5189f1c
Nils Müller <tyriis@users.noreply.github.com>
2024-10-28 23:51:44 +0100

    feat(nixos): adjust folder permisions

--- infra/nixos/hosts/kube-nas/configuration.nix
+++ infra/nixos/hosts/kube-nas/configuration.nix
@@ -127,18 +127,18 @@
   # create folder
   # https://discourse.nixos.org/t/adding-folders-and-scripts/5114/4
   systemd.tmpfiles.rules = [
-    "d /mnt/volume1/backup 0700 root root"
+    "d /mnt/volume1/backup 0750 root users"
     "d /mnt/volume1/backup/nils 0770 nils users"
     "d /mnt/volume1/backup/jasmin 0770 jasmin users"
     "d /mnt/volume1/backup/alex 0770 alex users"
     "d /mnt/volume1/backup/dominik 0770 dominik users"
-    "d /mnt/volume1/data 0700 root root"
+    "d /mnt/volume1/data 0750 root users"
     "d /mnt/volume1/data/csi 0770 kube users"
     "d /mnt/volume1/data/downloads 0770 root users"
     "d /mnt/volume1/data/scans 0770 root users"
     "d /mnt/volume1/data/syncthing 0770 root users"
     "d /mnt/volume1/data/media 0770 root users"
-    "d /mnt/volume1/home 0700 root root"
+    "d /mnt/volume1/home 0750 root users"
     "d /mnt/volume1/home/nils 0770 nils users"
     "d /mnt/volume1/home/jasmin 0770 jasmin users"
     "d /mnt/volume1/home/alex 0770 alex users"
