9805718fb2875435383c2c065c1fc1324de80bd6
Nils Müller <tyriis@users.noreply.github.com>
2025-04-07 23:43:27 +0200

    fix(atlantis): vault config injector

--- kubernetes/base/apps/atlantis/scripts/vault_config.sh
+++ kubernetes/base/apps/atlantis/scripts/vault_config.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 # This script replaces the ATLANTIS_INJECT_VAULT_CONFIG marker with vault auth_login configuration
 
-# Print directory contents for debugging
-echo "Current directory content:"
-cat terraform/provider.tf
-
 # Define the replacement text
-replacement='  auth_login {\n    path = "auth/kubernetes/login"\n    parameters = {\n      role = "talos-flux-atlantis"\n      jwt  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")\n    }\n  }'
+replacement='auth_login {\n    path = "auth/kubernetes/login"\n    parameters = {\n      role = "talos-flux-atlantis"\n      jwt  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")\n    }\n  }'
 
 # Find provider.tf files containing both "provider \"vault\"" and the marker
-for provider_file in $(grep -l 'provider "vault"' $(find . -name "terraform/provider.tf" -o -name "terraform/providers.tf") | xargs grep -l 'ATLANTIS_INJECT_VAULT_CONFIG'); do
+for provider_file in $(grep -l 'provider "vault"' $(find . -name "provider.tf" -o -name "providers.tf") | xargs grep -l 'ATLANTIS_INJECT_VAULT_CONFIG'); do
   echo "Found vault provider in $provider_file, injecting auth_login configuration..."
 
   # Use sed to perform the replacement
