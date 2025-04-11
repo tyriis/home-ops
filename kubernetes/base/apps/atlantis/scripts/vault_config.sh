220093d23ce2f7e5347d8d62b1a97903bfc02e52
Nils Müller <tyriis@users.noreply.github.com>
2025-04-11 23:14:12 +0200

    fix(atlantis): use kubernetes token for vault

--- kubernetes/base/apps/atlantis/scripts/vault_config.sh
+++ kubernetes/base/apps/atlantis/scripts/vault_config.sh
@@ -2,8 +2,8 @@
 # This script replaces the ATLANTIS_INJECT_VAULT_CONFIG marker with vault auth_login configuration
 
 # Define the replacement text
-# replacement='auth_login {\n    path = "auth/kubernetes/login"\n    parameters = {\n      role = "homeops"\n      jwt  = file("/var/run/secrets/kubernetes.io/serviceaccount/token")\n    }\n  }'
-replacement='auth_login {\n    path = "auth/approle/login"\n    parameters = {\n      role_id = "\${VAULT_ROLE_ID}"\n      secret_id = "\${VAULT_SECRET_ID}"\n    }\n  }'
+echo $BASE_REPO_NAME
+replacement="auth_login {\n    path = \"auth/kubernetes/login\"\n    parameters = {\n      role = \"$BASE_REPO_NAME\"\n      jwt  = file(\"/var/run/secrets/kubernetes.io/serviceaccount/token\")\n    }\n  }"
 
 # Find provider.tf files containing both "provider \"vault\"" and the marker
 for provider_file in $(grep -l 'provider "vault"' $(find . -name "provider.tf" -o -name "providers.tf") | xargs grep -l 'ATLANTIS_INJECT_VAULT_CONFIG'); do
